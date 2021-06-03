a9b783ad4167794b0ecd1e748c800468b60785e6
David Brais <2095059@cshawi.ca>
2021-06-03 10:45:46 -0400

    enlever les Href dans le html.

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+/* initialisation du json */
+
 var request = new XMLHttpRequest();
 request.open("GET", "../candies.json", false);
 request.send(null);
 var my_JSON_object = JSON.parse(request.responseText);
-console.log(my_JSON_object);
+
+/*initialisation des areas */
+const areas = document.getElementsByTagName("area");
+const mapArea = document.getElementsByName("image-map")[0];
+for(i=0;i<20;i++)
+{
+ areas[i].id = "candy"+i;
+}
+
+mapArea.addEventListener("click", function(event){
+    clickBonbon(event.target);
+    return false;
+})
+
+function clickBonbon(element)
+{
+    console.log(element.id);
+}
