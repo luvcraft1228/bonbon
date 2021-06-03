71a0d802054759c405ecef590fb6dcd53e78abe0
David Brais <2095059@cshawi.ca>
2021-06-03 11:13:09 -0400

    ajout des fonctions de crud et local storage

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
 /* initialisation du json */
+let internal = window.localStorage;
+let candiesObject = [];
+if(internal.getItem("candies")==null){
+    var request = new XMLHttpRequest();
+    request.open("GET", "../candies.json", false);
+    request.send(null);
+    internal.setItem("candies", request.responseText);
+    candiesObject = JSON.parse(request.responseText);
+} else {
+    candiesObject = JSON.parse(internal.getItem("candies"));
+}
 
-var request = new XMLHttpRequest();
-request.open("GET", "../candies.json", false);
-request.send(null);
-var my_JSON_object = JSON.parse(request.responseText);
-
+console.log(internal);
 /*initialisation des areas */
 const areas = document.getElementsByTagName("area");
 const mapArea = document.getElementsByName("image-map")[0];
 for(i=0;i<20;i++)
 {
- areas[i].id = "candy"+i;
+ areas[i].id = i;
 }
 
 mapArea.addEventListener("click", function(event){
     clickBonbon(event.target);
+    
     return false;
 })
 
 function clickBonbon(element)
 {
     console.log(element.id);
+    let activeCandy = getCandyById(element.id);
+    ShowName(activeCandy);
+    ShowPrice(activeCandy);
+    GetQty(activeCandy);
+}
+
+function getCandyById(id){
+    return candiesObject.find(candy => candy.id == id);
+
+}
+
+function ShowPrice(candy){
+    console.log(`ShowPrice : ${candy.price}`);
+}
+
+function ShowName(candy){
+    console.log(candy.name);
+}
+
+function GetQty(candy){
+    console.log(candy.qty);
 }
